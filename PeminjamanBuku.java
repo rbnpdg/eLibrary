f6a849d0c86a7e4f206a74882bcaf69ca651328a
DevinaCitra <112444724+DevinaCitra@users.noreply.github.com>
2023-11-29 23:15:25 +0700

    PeminjamanBuku.java

--- PeminjamanBuku.java
+++ PeminjamanBuku.java
@@ -9,16 +9,20 @@ import java.util.Date;
  *
  * @author adita
  */
+
+//kelas ini merepresentasikan peminjaman buku yang berisi tanggal pinjam, isbn, dan durasi peminjaman
 public class PeminjamanBuku {
     private Date tanggal;
     private String isbn;
     private int durasi;
     
+    //konstruktor untuk membuat objek PeminjamanBuku dengan parameter tanggal dan durasi
     public PeminjamanBuku(Date tanggal, int durasi){
         this.tanggal = tanggal;
         this.durasi = durasi;
     }
-
+    
+    //getter dan setter tanggal
     public Date getTanggal() {
         return tanggal;
     }
@@ -26,7 +30,8 @@ public class PeminjamanBuku {
     public void setTanggal(Date tanggal) {
         this.tanggal = tanggal;
     }
-
+    
+    //getter dan setter durasi
     public int getDurasi() {
         return durasi;
     }
@@ -34,7 +39,8 @@ public class PeminjamanBuku {
     public void setDurasi(int durasi) {
         this.durasi = durasi;
     }
-
+    
+    //getter dan setter isbn
     public String getIsbn() {
         return isbn;
     }
