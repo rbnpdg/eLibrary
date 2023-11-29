e0d526483487b11a247e8956b2d997c857494e71
DevinaCitra <112444724+DevinaCitra@users.noreply.github.com>
2023-11-29 23:13:53 +0700

    AnggotaPerpustakaan.java

--- AnggotaPerpustakaan.java
+++ AnggotaPerpustakaan.java
@@ -11,19 +11,23 @@ import java.util.List;
  *
  * @author adita
  */
+
+//kelas ini merepresentasikan anggota perpustakaan yang berisi informasi mengenai nama, nomor anggota, alamat dan riwayat peminjaman buku.
 public class AnggotaPerpustakaan {
     private String nama;
     private String nomoranggota;
     private String alamat;
     private List<PeminjamanBuku> riwayat;
     
+    //konstruktor untuk membuat objek AnggotaPerpustakaan dengan parameter nama, nomoranggota, alamat, dan sejaranpeminjaman
     public AnggotaPerpustakaan(String nama, String nomoranggota, String alamat, List<PeminjamanBuku> sejarahpeminjaman){
         this.nama = nama;
         this.nomoranggota = nomoranggota;
         this.alamat = alamat;
         this.riwayat = riwayat;
     }
-
+    
+    //getter setter nama anggota
     public String getNama() {
         return nama;
     }
@@ -32,6 +36,7 @@ public class AnggotaPerpustakaan {
         this.nama = nama;
     }
 
+    //getter setter nomor anggota
     public String getNomoranggota() {
         return nomoranggota;
     }
@@ -40,6 +45,7 @@ public class AnggotaPerpustakaan {
         this.nomoranggota = nomoranggota;
     }
 
+    //getter setter alamat
     public String getAlamat() {
         return alamat;
     }
@@ -47,7 +53,8 @@ public class AnggotaPerpustakaan {
     public void setAlamat(String alamat) {
         this.alamat = alamat;
     }
-
+    
+    //getter dan setter riwayat peminjaman buku
     public List<PeminjamanBuku> getRiwayat() {
         return riwayat;
     }
@@ -56,6 +63,7 @@ public class AnggotaPerpustakaan {
         this.riwayat = riwayat;
     }
     
+    //metode untuk mencari buku oleh anggota
     public void cariBuku(Buku buku) {
         
     }
