66b0344a918255c7fc2b8d2ced20e9c188e6f80c
DevinaCitra <112444724+DevinaCitra@users.noreply.github.com>
2023-11-29 23:18:14 +0700

    Pustakawan.java

--- Pustakawan.java
+++ Pustakawan.java
@@ -11,15 +11,20 @@ import java.util.List;
  *
  * @author adita
  */
+
+//kelas ini merepresentasikan pustakawan yang berisi informasi yaitu id dan nama pustakawan
+//serta terdapat metode tambah buku dan hapus buku
 public class Pustakawan {
     private String id;
     private String nama;
     
+    //konstruktor untuk membuat objek Pustakawan dengan parameter id dan nama
     public Pustakawan(String id, String nama){
         this.id = id;
         this.nama = nama;
     }
-
+    
+    //getter dan setter nama
     public String getNama() {
         return nama;
     }
@@ -27,7 +32,8 @@ public class Pustakawan {
     public void setNama(String nama) {
         this.nama = nama;
     }
-
+    
+    //getter dan setter id
     public String getId() {
         return id;
     }
@@ -36,11 +42,13 @@ public class Pustakawan {
         this.id = id;
     }
     
+    //metode untuk menambahkan buku dari perpustakaan
     public void tambahBuku(Buku buku, List<Buku> daftarBuku) {
         daftarBuku.add(buku);
         System.out.println("Buku dengan judul " + buku.getJudul() + " berhasil ditambahkan.");
     }
     
+    //metode untuk menghapus buku dari perpustakaan
     public void hapusBuku (Buku buku){
         
     }
