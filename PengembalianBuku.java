15bd4c42db9df3480d0fde8a1a100822fb9ad812
DevinaCitra <112444724+DevinaCitra@users.noreply.github.com>
2023-11-29 23:15:51 +0700

    PengembalianBuku.java

--- PengembalianBuku.java
+++ PengembalianBuku.java
@@ -11,16 +11,20 @@ import java.util.Date;
  *
  * @author adita
  */
+
+//kelas ini merepresentasikan pengembalian buku yang berisi tanggal, isbn, dan durasi peminjaman
 public class PengembalianBuku {
     private Date tanggal;
     private String isbn;
     private int durasiPeminjaman;
     
+    //konstruktor untuk membuat objek PengembalianBuku dengan parameter tanggal dan durasi peminjaman
     public PengembalianBuku(Date tanggal, int durasiPeminjaman){
         this.tanggal = tanggal;
         this.durasiPeminjaman = durasiPeminjaman;
     }
-
+    
+    //getter dan setter tanggal
     public Date getTanggal() {
         return tanggal;
     }
@@ -29,6 +33,7 @@ public class PengembalianBuku {
         this.tanggal = tanggal;
     }
 
+    //getter dan setter DurasiPeminjaman
     public int getDurasiPeminjaman() {
         return durasiPeminjaman;
     }
@@ -36,7 +41,8 @@ public class PengembalianBuku {
     public void setDurasiPeminjaman(int durasi) {
         this.durasiPeminjaman = durasi;
     }
-
+    
+    //getter dan setter isbn
     public String getIsbn() {
         return isbn;
     }
