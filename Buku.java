360ef76c932ccbc9cb0d6d7ec4d6ec8ea6f5a1ed
DevinaCitra <112444724+DevinaCitra@users.noreply.github.com>
2023-11-29 23:14:23 +0700

    Buku.java

--- Buku.java
+++ Buku.java
@@ -9,12 +9,17 @@ package perpustakaan;
  *
  * @author adita
  */
+
+// Kelas ini mempresentasikan buku dalam perpustakaan yang berisi informasi mengenai buku seperti isbn, judul, pengarang dan ketersediaan.
+// Serta terdapat metode untuk menambah dan menghapus buku dari koleksi perpustakaan
+
 public class Buku {
     private String isbn;
     private String judul;
     private String pengarang;
     private boolean ketersediaan;
-    
+
+    //konstruktor untuk membuat objek dengan parameter isbn, judul, pengarang dan ketersediaan
     public Buku(String isbn, String judul, String pengarang, boolean ketersediaan){
         this.isbn = isbn;
         this.judul = judul;
@@ -23,12 +28,14 @@ public class Buku {
         
     }
     
+    //konstruktor untuk membuat objek dengan parameter isbn, judul, pengarang dan nomorISBN
     public Buku(String isbn, String judul, String pengarang, String nomorISBN){
         this.isbn = isbn;
         this.judul = judul;
         this.pengarang = pengarang;
     }
-
+    
+    // getter dan setter judul buku
     public String getJudul() {
         return judul;
     }
@@ -36,7 +43,8 @@ public class Buku {
     public void setJudul(String judul) {
         this.judul = judul;
     }
-
+    
+    //getter dan setter pengarang
     public String getPengarang() {
         return pengarang;
     }
@@ -45,6 +53,7 @@ public class Buku {
         this.pengarang = pengarang;
     }
 
+    //getter dan setter isbn
     public String getIsbn() {
         return isbn;
     }
@@ -52,7 +61,8 @@ public class Buku {
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
-
+    
+    //getter dan setter ketersiaan buku
     public boolean isKetersediaan() {
         return ketersediaan;
     }
@@ -61,12 +71,14 @@ public class Buku {
         this.ketersediaan = ketersediaan;
     }
     
+    //metode untuk menambah koleksi buku di perpustakaan
     public void menambahKoleksi(){
         System.out.println("ISBN        : " + isbn);
         System.out.println("Judul       : " + judul);
         System.out.println("Pengarang   : " + pengarang);
     }
     
+    //metode untuk menghapus koleksi buku di perpustakaan
     public void menghapusKoleksi(){
         
     }
